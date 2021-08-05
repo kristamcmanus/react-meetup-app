6aab1c0dbbe1c81798a4484d0bb705a9906199a7
kristamcmanus <k.mcmanus310@gmail.com>
2021-08-05 14:59:35 -0400

    Integrate Firebase Realtime Database as the api

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = userRef();
   const addressInputRef = userRef();
@@ -22,6 +22,8 @@ function NewMeetupForm() {
       address: enteredAddress,
       description: enteredDescription,
     };
+
+    props.onAddMeetup(meetupData);
   }
   return (
     <Card>
